9885f3cf8e706c24421c0adc045c15f1a3294f01
DeathNaught-GitHub <coding.ayush@gmail.com>
2020-07-01 05:17:12 +0530

    home page front ready day 1

---
+++ frontend/resources/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $("body").scroll(function() {
+        var $nav = $(".nav");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        $nav.toggleClass('over', $(this).scrollTop() > 1900);
+    })
+})
--- views/resources/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $("body").scroll(function() {
-        var $nav = $(".nav");
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-        $nav.toggleClass('overview', $(this).scrollTop() > 2000);
-    })
-})
