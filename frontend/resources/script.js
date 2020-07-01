6927a36a592fdd69332565ec9172b93cabfa3b30
DeathNaught-GitHub <coding.ayush@gmail.com>
2020-07-01 23:44:06 +0530

    Dashboard UI Desktop view DAY 2

--- frontend/resources/script.js
+++ frontend/resources/script.js
@@ -5,3 +5,35 @@ $(document).ready(function() {
         $nav.toggleClass('over', $(this).scrollTop() > 1900);
     })
 })
+
+/* ------------------------ Time ------------------------ */
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var time = h;
+    if (h != 12) h = h % 12;
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    h = checkTime(h);
+    m = checkTime(m);
+    s = checkTime(s);
+    if (time >= 12)
+        document.getElementById('time').innerHTML =
+        h + ":" + m + ":" + s + " PM";
+    else
+        document.getElementById('time').innerHTML =
+        h + ":" + m + ":" + s + " AM";
+    var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+    if (i < 10) { i = "0" + i }; // add zero in front of numbers < 10
+    return i;
+}
+
+/* ------------------------ Drag ------------------------ */
+$(function() {
+    $("#sortable").sortable();
+    $("#sortable").disableSelection();
+});
