1d35b67a28e4347d945f03de99c77c7d1d0f006f
DeathNaught-GitHub <coding.ayush@gmail.com>
2020-07-02 13:20:12 +0530

    MongoDB Schemas

--- model/User.js
+++ model/User.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    avatar: { type: String, default: 'Sauron' },
     sessionToken: { type: String, default: null }
 }, {
     timestamps: true
