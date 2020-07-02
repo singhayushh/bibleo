bb11cb5badb01f03b3ef4c4e03a1a6ee68e8808c
DeathNaught-GitHub <coding.ayush@gmail.com>
2020-07-02 13:02:11 +0530

    Mongo Schemas

--- model/Book.js
+++ model/Book.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
     name: { type: String, required: true, unique: true },
     link: { type: String, required: true },
-    cover: { type: String },
+    cover: { type: String, default: 'default' },
     favourite: { type: Boolean, default: false },
     have_read: { type: Boolean, default: false },
     reading_now: { type: Boolean, default: false },
--- model/User.js
+++ model/User.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    sessionToken: { type: String, default: null }
 }, {
     timestamps: true
 });
