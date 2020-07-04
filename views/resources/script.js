16e333d7c41582f12fbea55374511df7e06fb93f
DeathNaught-GitHub <coding.ayush@gmail.com>
2020-07-04 19:05:50 +0530

    Frontend Interface

--- views/resources/script.js
+++ views/resources/script.js
@@ -48,16 +48,31 @@ $('div.close-btn').click(function() {
     $(".popup").css({ "display": "none" });
 });
 
+ $(".book-overlay").hover(
+function() {
+        $(this).find(".foot, .toolbar").css({"display": "flex"});        
+},
+function() {
+    $(this).find(".foot, .toolbar").css("display", "none");
+});
+
 
 /* --------------------- Book-Cover --------------------- */
 
 $(window).on("load", function() {
     var $pic = $(".image").text().trim().split(" ");
+    var $link = $(".get-link").text().trim().split(" ");
+    var $id = $(".get-id").text().trim().split(" ");
     for(var $i = 0; $i < $pic.length; $i++) {
         if ($pic[$i] === "default") {
-            $pic[$i] = `./resources/img/book-cover/5.png`
+            $pic[$i] = `./resources/img/book-cover/default.png`
         }
-        $(`li:nth-child(${$i + 1}) img`).attr("src", $pic[$i].toString());
+        try {
+        $(`li:nth-child(${$i + 1}) a`).attr("href", $link[$i].toString());
+        $(`.book_id:eq(${$i})`).val($id[$i]);
+        $(`li:nth-child(${$i + 1}) a`).css({"background-image": `url(${$pic[$i].toString()})`, "background-size": "100% 100%", "height": "100%", "width": "100%"});
+        }
+        catch(err){alert(err)}
     }
     
 });
